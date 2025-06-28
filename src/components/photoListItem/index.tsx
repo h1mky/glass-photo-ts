9cd471f5853747049dbd8d7f21beda5ca103f925
zvezd <adelevnikita@gmail.com>
2025-06-28 15:10:38 +0200

    forgot add phoo list item

---
+++ src/components/photoListItem/index.tsx
@@ -0,0 +1,29 @@
+interface Photo {
+  id: number;
+  imageUrl: string;
+  username: string;
+  title: string;
+}
+
+const PhotoListItem: React.FC<{ photo: Photo }> = ({ photo }) => {
+  return (
+    <div className="photo-list-item">
+      <div className="photo-item-img-wrapper">
+        <img
+          src={photo.imageUrl}
+          alt={photo.title}
+          className="photo-item-img"
+        />
+
+        <div className="photo-item-overlay">
+          <div className="photo-item-info">
+            <div className="photo-username">{photo.username}</div>
+            <div className="photo-description">{photo.title}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoListItem;
