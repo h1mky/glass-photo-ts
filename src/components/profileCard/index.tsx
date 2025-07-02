5095fd30a9b4f7a124867fa533da385e96969343
zvezd <adelevnikita@gmail.com>
2025-07-02 18:51:13 +0200

    add fetch request for a user info like bio avatar and more

--- src/components/profileCard/index.tsx
+++ src/components/profileCard/index.tsx
@@ -1,18 +1,46 @@
 import "./profileCard.css";
+import { fetchUserProfile } from "../../services/UserService/service";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+interface UserDesOrImg {
+  String: string;
+  Valid: boolean;
+}
+
+interface User {
+  id: number;
+  username: string;
+  user_img: UserDesOrImg;
+  description: UserDesOrImg;
+}
 
 const ProfileCard = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetchUserProfile(Number(id));
+      setUser(response.data);
+    };
+    fetchData();
+  }, [id]);
+
+  const userDescValid = user?.description.Valid
+    ? user?.description.String
+    : "user has no bio";
+
   return (
     <section className="profile-card">
       <div className="profile-hero">
-        <img
-          src="https://theafictionado.wordpress.com/wp-content/uploads/2017/01/luckystar.png"
-          className="hero-image"
-        />
+        <img src={user?.user_img.String} className="hero-image" />
       </div>
 
       <div className="profile-info">
-        <h1 className="profile-name">@zvezd</h1>
-        <p className="profile-bio pb-3">dsa</p>
+        <h1 className="profile-name">@{user?.username}</h1>
+        <p className="profile-bio pb-3">{userDescValid}</p>
 
         <div className="profile-nav">
           <span className="nav-item active">Photos</span>
