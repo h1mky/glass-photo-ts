9f1e62d6cba526786afcfe9b6cdea3a45c5fd202
zvezd <adelevnikita@gmail.com>
2025-07-03 15:12:13 +0200

    add panel with sign up

--- src/services/UserService/service.ts
+++ src/services/UserService/service.ts
@@ -1,5 +1,20 @@
 import { request } from "../../hook/http.hook";
 
+export interface SignUpResponse {
+  email: string;
+  username: string;
+  password: string;
+  userPhoto: string;
+}
+
 export const fetchUserProfile = (ID: number) => {
   return request(`http://localhost:3000/user/${ID}`, "GET");
+};
+
+export const fetchSignUp = (userData: SignUpResponse) => {
+  return request(
+    `http://localhost:3000/sign-up`,
+    "POST",
+    JSON.stringify(userData)
+  );
 };
