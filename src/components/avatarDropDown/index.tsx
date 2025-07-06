b417e980e2e294a4de89859618e57c4cf418a0f1
zvezd <adelevnikita@gmail.com>
2025-07-06 13:14:44 +0200

    restore localstorage in press log out button

--- src/components/avatarDropDown/index.tsx
+++ src/components/avatarDropDown/index.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Avatar, Menu, MenuItem, IconButton } from "@mui/material";
-
+import {
+  Avatar,
+  Menu,
+  MenuItem,
+  IconButton,
+  Tooltip,
+  Box,
+  ListItemIcon,
+} from "@mui/material";
+import { Settings, LogOut } from "lucide-react";
 import type { UserMain } from "../../redux/userSlice/type";
 
 const AvatarDropdown: React.FC<UserMain> = (user) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const navigate = useNavigate();
 
-  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget); // открытие меню
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -21,33 +29,118 @@ const AvatarDropdown: React.FC<UserMain> = (user) => {
     handleClose();
   };
 
+  const restoreLocalStorageToken = () => {
+    localStorage.removeItem("authToken");
+    navigate(0);
+  };
+
+  const hoverColor = "#8ab4f8";
+
   return (
     <>
-      <IconButton onClick={handleOpen} size="small">
-        <Avatar src={user.user_img} alt="user" />
-      </IconButton>
-
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            aria-label="Account settings"
+          >
+            <Avatar
+              sx={{ width: 40, height: 40 }}
+              src={user.user_img || undefined}
+            ></Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
       <Menu
         anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
+        id="account-menu"
+        open={open}
         onClose={handleClose}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        transformOrigin={{ vertical: "top", horizontal: "center" }}
-        PaperProps={{
-          sx: {
-            backgroundColor: "#18181a",
-            color: "#8e8e93",
-            mt: 1,
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              backgroundColor: "#18181A",
+              color: "#fff",
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&::before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: "50%",
+                transform: "translateX(50%) translateY(-50%) rotate(45deg)",
+                width: 10,
+                height: 10,
+                bgcolor: "#323441",
+                zIndex: 0,
+              },
+            },
           },
         }}
+        transformOrigin={{ horizontal: "center", vertical: "top" }}
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+        disableScrollLock
       >
-        <MenuItem onClick={() => handleNavigate(`/user/${user.id}`)}>
-          Профиль
+        <MenuItem
+          onClick={() => handleNavigate(`/user/${user.id}`)}
+          sx={{
+            "&:hover .MuiAvatar-root": {
+              opacity: 0.8,
+            },
+            "&:hover .menu-text": {
+              color: hoverColor,
+            },
+          }}
+        >
+          <Avatar src={user.user_img || undefined} />
+          <span className="menu-text">Profile</span>
         </MenuItem>
-        <MenuItem onClick={() => handleNavigate("/settings")}>
-          Настройки
+        <MenuItem
+          onClick={() => handleNavigate("/settings")}
+          sx={{
+            "&:hover .MuiListItemIcon-root": {
+              color: hoverColor,
+            },
+            "&:hover .menu-text": {
+              color: hoverColor,
+            },
+          }}
+        >
+          <ListItemIcon>
+            <Settings size={18} color="#ffffff" />
+          </ListItemIcon>
+          <span className="menu-text">Settings</span>
         </MenuItem>
-        <MenuItem onClick={() => handleNavigate("/logout")}>Выйти</MenuItem>
+        <MenuItem
+          onClick={() => restoreLocalStorageToken()}
+          sx={{
+            "&:hover .MuiListItemIcon-root": {
+              color: hoverColor,
+            },
+            "&:hover .menu-text": {
+              color: hoverColor,
+            },
+          }}
+        >
+          <ListItemIcon>
+            <LogOut size={18} color="#ffffff" />
+          </ListItemIcon>
+          <span className="menu-text">Logout</span>
+        </MenuItem>
       </Menu>
     </>
   );
