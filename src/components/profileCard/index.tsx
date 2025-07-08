d9afa506dc8a79fec049d638964780054473abe5
zvezd <adelevnikita@gmail.com>
2025-07-08 15:41:39 +0200

    add verification on user page for 404 page

--- src/components/profileCard/index.tsx
+++ src/components/profileCard/index.tsx
@@ -3,6 +3,8 @@ import { fetchUserProfile } from "../../services/UserService/service";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import NotFoundPage from "../../pages/page404";
+
 import type { DescriptionOrImage } from "../../redux/postsSlice/types";
 
 interface User {
@@ -30,20 +32,26 @@ const ProfileCard = () => {
     : "user has no bio";
 
   return (
-    <section className="profile-card">
-      <div className="profile-hero">
-        <img src={user?.user_img} className="hero-image" />
-      </div>
+    <>
+      {!user?.id ? (
+        <NotFoundPage />
+      ) : (
+        <section className="profile-card">
+          <div className="profile-hero">
+            <img src={user?.user_img} className="hero-image" />
+          </div>
 
-      <div className="profile-info">
-        <h1 className="profile-name">@{user?.username}</h1>
-        <p className="profile-bio pb-3">{userDescValid}</p>
+          <div className="profile-info">
+            <h1 className="profile-name">@{user?.username}</h1>
+            <p className="profile-bio pb-3">{userDescValid}</p>
 
-        <div className="profile-nav">
-          <span className="nav-item active">Photos</span>
-        </div>
-      </div>
-    </section>
+            <div className="profile-nav">
+              <span className="nav-item active">Photos</span>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 export default ProfileCard;
